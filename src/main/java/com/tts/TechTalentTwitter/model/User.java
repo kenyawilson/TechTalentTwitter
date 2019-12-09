42b4827abb9ca064dc7bab52449075b661ed0ca1
kenyawilson <kenyawilson@users.noreply.github.com>
2019-12-09 09:01:18 -0500

    added a user class

---
+++ src/main/java/com/tts/TechTalentTwitter/model/User.java
@@ -0,0 +1,39 @@
+package com.tts.TechTalentTwitter.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private Long id;
+
+	private String email;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private int active;
+
+	@CreationTimestamp 
+	private Date createdAt;
+}
+
